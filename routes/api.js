9220ae134884b7024ff84dc909d1772f7af4d2df
Don <nguyentuand85@gmail.com>
2020-05-01 00:00:59 -0700

    deploy to heroku

--- routes/api.js
+++ routes/api.js
@@ -1,54 +1,54 @@
 const Workout = require("../models/Workout");
 
 module.exports = function (app) {
-    app.get("/api/workouts/range", (req, res) => {
-        Workout.find({})
-            .sort({
-                date: -1
-            })
-            .limit(7)
-            .then(db => {
-                res.json(db)
-            })
-            .catch(err => {
-                res.status(400).json(err);
-            });
-    })
-    app.get("/api/workouts", (req, res) => {
-        Workout.find({})
-            .sort({
-                date: -1
-            })
-            .then(db => {
-                res.json(db)
-            })
-            .catch(err => {
-                res.status(400).json(err);
-            });
-    })
-    app.post('/api/workouts', ({ body }, res) => {
-        Workout.create(body)
-            .then((dbWorkout) => {
-                res.json(dbWorkout)
-            })
-            .catch((err) => {
-                res.status(400).json(err)
-            })
-    })
-    app.put('/api/workouts/:id', ({ body, params }, res) => {
-        Workout.updateOne({ _id: params.id }, { $push: { exercises: body } })
-            .then((dbWorkout) => {
-                res.json(dbWorkout)
-            })
-            .catch((err) => {
-                res.status(400).json(err)
-            })
-    })
-    app.delete("/api/posts/:id", (req, res) => {
-        Workout.deleteOne({ 
-            id: req.params.id
-        }).then(deleteworkout => {
-          res.json(deleteworkout);
-        });
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+      .sort({
+        date: -1,
+      })
+      .limit(7)
+      .then((db) => {
+        res.json(db);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
       });
+  });
+  app.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .sort({
+        date: -1,
+      })
+      .then((db) => {
+        res.json(db);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+  app.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+  app.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.updateOne({ _id: params.id }, { $push: { exercises: body } })
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+  app.delete("/api/posts/:id", (req, res) => {
+    Workout.deleteOne({
+      id: req.params.id,
+    }).then((deleteWorkout) => {
+      res.json(deleteWorkout);
+    });
+  });
 };
